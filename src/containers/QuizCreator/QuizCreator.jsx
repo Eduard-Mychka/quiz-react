9a659c63d3c72296925203eaec3b9d0808cc2461
ed_nikolaevich <edmychka@gmail.com>
2021-07-19 17:21:31 +0300

    d

--- src/containers/QuizCreator/QuizCreator.jsx
+++ src/containers/QuizCreator/QuizCreator.jsx
@@ -139,7 +139,7 @@ export default class QuizCreator extends Component {
     return (
       <div className="quiz-creator"> 
         <div className="home">
-            <a href="/"><i className="fas fa-home" /></a>
+            <a href="/quiz-react/"><i className="fas fa-home" /></a>
           </div>
         <div className="containere">
           <Slide top>
